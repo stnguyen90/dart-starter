846e159542fe9bbeec59be795cfa2a4ec9ce9c91
Steven Nguyen <1477010+stnguyen90@users.noreply.github.com>
2023-10-13 17:25:05 -0700

    Update main.dart
    log env

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 Future<dynamic> main(final context) async {
+  context.log(Platform.environment);
   return context.res.json({
     'environment': Platform.environment,
   });
