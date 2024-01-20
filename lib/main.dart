6a073a764fc4ccc1d7312d524b7182867503440a
Steven Nguyen <1477010+stnguyen90@users.noreply.github.com>
2024-01-19 16:26:44 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 import 'dart:io';
 
 Future<dynamic> main(final context) async {
+  context.log("env:");
   context.log(Platform.environment);
+  context.log("headers:");
   context.log(context.req.headers);
+  context.log("bodyRaw:");
   context.log(context.req.bodyRaw);
   return context.res.json({
     'env': Platform.environment,
