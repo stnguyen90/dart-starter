da57a32b7bdbbd400a825f233258f56095989001
Steven Nguyen <1477010+stnguyen90@users.noreply.github.com>
2024-12-12 11:23:19 -0800

    Update main.dart

--- functions/first/lib/main.dart
+++ functions/first/lib/main.dart
@@ -4,11 +4,18 @@ import 'dart:convert';
 
 Future<dynamic> main(final context) async {
   final res = {
+    'scheme': context.req.scheme,
+    'method': context.req.method,
+    'url': context.req.url,
+    'host': context.req.host,
+    'port': context.req.port,
+    'path': context.req.path,
     'headers': context.req.headers,
     'environment': Platform.environment,
     'body': context.req.bodyRaw
   };
 
-  context.log(jsonEncode(res));
+  JsonEncoder encoder = new JsonEncoder.withIndent('  ');
+  context.log(encoder.convert(res));
   return context.res.json(res);
 }
