b2d9d176e9fda225268897192e2525bcd3c6b716
Steven Nguyen <1477010+stnguyen90@users.noreply.github.com>
2025-05-17 21:07:10 -0700

    Update main.dart

--- functions/first/lib/main.dart
+++ functions/first/lib/main.dart
@@ -17,5 +17,9 @@ Future<dynamic> main(final context) async {
 
   JsonEncoder encoder = new JsonEncoder.withIndent('  ');
   context.log(encoder.convert(res));
-  return context.res.json(res);
+  return context.res.json(res, 200, {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+  });
 }
